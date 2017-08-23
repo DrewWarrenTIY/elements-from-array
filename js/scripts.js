15f9798288570c0fbc4b6253fb90c6577ca085d7
DrewWarrenTIY <drewwarrentiy@gmail.com>
2017-08-23 10:28:54 -0600

    Working Daplie Example

--- js/scripts.js
+++ js/scripts.js
@@ -41,26 +41,58 @@ var exampleObj = {
   "type": "binaryswitches"
 }
 
+var exampleObjArray = [{
+  "network-id":"093B",
+  "endpoint":"01",
+  "euid64":"000D6F000B2E5846",
+  "in-clusters":["Basic","Identify","Groups","Scenes","On/Off","Electricity Measurement","Diagnostics","FC03[Unknown/Non-Standard]"],
+  "out-clusters":["OTA Update (Non HA/ZCL)"],
+  "profile":"Home Automation (HA)",
+  "name": "Iris Plug One",
+  "device": "Iris Plug",
+  "type": "binaryswitches"
+}, {
+  "network-id":"093B",
+  "endpoint":"01",
+  "euid64":"000D6F000B2E5846",
+  "in-clusters":["Basic","Identify","Groups","Scenes","On/Off","Electricity Measurement","Diagnostics","FC03[Unknown/Non-Standard]"],
+  "out-clusters":["OTA Update (Non HA/ZCL)"],
+  "profile":"Home Automation (HA)",
+  "name": "Leviton Plug One",
+  "device": "Leviton Plug",
+  "type": "binaryswitches"
+}, {
+  "network-id":"093B",
+  "endpoint":"01",
+  "euid64":"000D6F000B2E5846",
+  "in-clusters":["Basic","Identify","Groups","Scenes","On/Off","Electricity Measurement","Diagnostics","FC03[Unknown/Non-Standard]"],
+  "out-clusters":["OTA Update (Non HA/ZCL)"],
+  "profile":"Home Automation (HA)",
+  "name": "Yale Lock One",
+  "device": "Yale Lock",
+  "type": "locks"
+}];
+
 console.log("exampleObj: ", exampleObj);
 
 function makeFirstDiv(divId, cssClass) {
   return '<div id="' + divId + '" class="dap-control-element dap-toggle-' + cssClass + '"> </div>';
 }
 
-function makeSecondDiv() {
-  return '<div class="dap-box"> </div>';
+function makeSecondDiv(id) {
+  return '<div class="dap-box ' + id + '-dap-box"> </div>';
 }
 
-function makeFirstP(text) {
-  return '<p class="dap-title">' + text + '</p>';
+function makeFirstP(id, text) {
+  return '<p class="dap-title ' + id + '-dap-title">' + text + '</p>';
 }
 
-function makeThirdDiv() {
-  return '<div class="div-three"> </div>';
+function makeThirdDiv(id) {
+  return '<div class="div-three ' + id + '-div-three"> </div>';
 }
 
-function makeSecondP(text) {
-  return '<p class="dap-box-text">' + text + '</p>';
+function makeSecondP(id, text) {
+  return '<p class="dap-box-text ' + id + '-dap-box-text">' + text + '</p>';
 }
 
 function makeFirstI(text) {
@@ -83,7 +115,7 @@ function elementFromObj(obj, number) {
   if(obj.device === "Yale Lock") {
     var divId = "doorBtn-" + number;
     var cssClass = "yale-lock";
-    var fontAwesomeClass = "lock";
+    var fontAwesomeClass = "unlock";
     var secondPText = "Unlocked";
   }
   console.log("makeFirstDiv: ", makeFirstDiv(divId, cssClass));
@@ -94,31 +126,28 @@ function elementFromObj(obj, number) {
 
   var secondDiv = document.createElement("div");
   document.querySelector('.dap-toggle-' + cssClass).appendChild(secondDiv);
-  secondDiv.outerHTML = makeSecondDiv();
+  secondDiv.outerHTML = makeSecondDiv(divId);
 
   var firstP = document.createElement("p");
   document.querySelector('.dap-toggle-' + cssClass).appendChild(firstP);
-  firstP.outerHTML = makeFirstP(obj.name);
+  firstP.outerHTML = makeFirstP(divId, obj.name);
 
   var thirdDiv = document.createElement("div");
-  document.querySelector('.dap-box').appendChild(thirdDiv);
-  thirdDiv.outerHTML = makeThirdDiv();
+  document.querySelector('.' + divId + '-dap-box').appendChild(thirdDiv);
+  thirdDiv.outerHTML = makeThirdDiv(divId);
 
   var secondP = document.createElement("p");
-  document.querySelector('.dap-box').appendChild(secondP);
-  secondP.outerHTML = makeSecondP(secondPText);
+  document.querySelector('.' + divId + '-dap-box').appendChild(secondP);
+  secondP.outerHTML = makeSecondP(divId, secondPText);
 
   var firstI = document.createElement("i");
-  document.querySelector(".div-three").appendChild(firstI);
+  document.querySelector('.' + divId + '-div-three').appendChild(firstI);
   firstI.outerHTML = makeFirstI(fontAwesomeClass);
 
 }
 
-elementFromObj(exampleObj, 1);
-// <div id="lightBtn" class="dap-control-element dap-toggle-iris-plug hidden">
-//           <div class="dap-box">
-//             <div><i class="fa fa-lightbulb-o fa-3x" aria-hidden="true"></i></div>
-//             <p class="dap-box-text">75%</p>
-//           </div>
-//           <p class="dap-title">Iris Plug One</p>
-//         </div>
+// elementFromObj(exampleObj, 1);
+
+exampleObjArray.forEach(function (c,i,a) {
+  elementFromObj(c,i);
+});
