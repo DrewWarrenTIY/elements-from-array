52a8db41bc0e8ee6e246e1bc481d5e5b3b9ccadc
DrewWarrenTIY <drewwarrentiy@gmail.com>
2017-09-13 17:43:12 -0600

    Elements from Objects

--- js/scripts.js
+++ js/scripts.js
@@ -60,8 +60,30 @@ function addToObjectArr() {
   age.value = '';
 }
 
-function makeFirstDiv(gender) {
-  return '<div class="first-div ' + gender + '"> </div>';
+var maleCount = 0;
+var femaleCount = 0;
+var otherCount = 0;
+
+function incrementGender(obj) {
+  if(obj.gender === "male") {
+    maleCount ++;
+  }
+  if(obj.gender === "female") {
+    femaleCount ++;
+  }
+  if(obj.gender === "other") {
+    otherCount ++;
+  }
+}
+
+function resetGenderCount() {
+  maleCount = 0;
+  femaleCount = 0;
+  otherCount = 0;
+}
+
+function makeFirstDiv(divId, gender) {
+  return '<div id="' + divId + '" class="first-div ' + gender + '"> </div>';
 }
 
 function makeSecondDiv() {
@@ -76,182 +98,52 @@ function makeSecondP(age) {
   return '<p class="age">' + age + '</p>';
 }
 
-function makeElementsFromObj() {
+function makeElementFromObj(obj, number) {
+  if(obj.gender === "male") {
+    var divId = "male" + number;
+  }
+  if(obj.gender === "female") {
+    var divId = "female" + number;
+  }
+  if(obj.gender === "other") {
+    var divId = "other" + number;
+  }
+
+  var firstDiv = document.createElement("div");
+  document.querySelector('.obj-element-container').appendChild(firstDiv);
+  firstDiv.outerHTML = makeFirstDiv(divId, obj.gender);
+
+  var secondDiv = document.createElement("div");
+  document.getElementById(divId).appendChild(secondDiv);
+  secondDiv.outerHTML = makeSecondDiv();
+
+  var firstP = document.createElement("div");
+  document.getElementById(divId).appendChild(secondDiv);
+  secondDiv.outerHTML = makeFirstP(obj.name);
+
+  var secondP = document.createElement("div");
+  document.getElementById(divId).appendChild(secondDiv);
+  secondDiv.outerHTML = makeSecondP(obj.age);
 
 }
 
+function makeElementsFromObj() {
+  objArr.forEach(function (c,i,a) {
+    incrementGender(c);
+    if (c.gender === "male") {
+      makeElementFromObj(c, maleCount);
+    }
+    if (c.gender === "female") {
+      makeElementFromObj(c, femaleCount);
+    }
+    if (c.gender === "other") {
+      makeElementFromObj(c, otherCount);
+    }
 
-//
-// var exampleObj = {
-//   "network-id":"093B",
-//   "endpoint":"01",
-//   "euid64":"000D6F000B2E5846",
-//   "in-clusters":["Basic","Identify","Groups","Scenes","On/Off","Electricity Measurement","Diagnostics","FC03[Unknown/Non-Standard]"],
-//   "out-clusters":["OTA Update (Non HA/ZCL)"],
-//   "profile":"Home Automation (HA)",
-//   "name": "Iris Plug One",
-//   "device": "Yale Lock",
-//   "type": "binaryswitches"
-// }
-//
-// var irisCount = 0;
-// var levitonCount = 0;
-// var yaleCount = 0;
-//
-// var exampleObjArray = [{
-//   "network-id":"093B",
-//   "endpoint":"01",
-//   "euid64":"000D6F000B2E5846",
-//   "in-clusters":["Basic","Identify","Groups","Scenes","On/Off","Electricity Measurement","Diagnostics","FC03[Unknown/Non-Standard]"],
-//   "out-clusters":["OTA Update (Non HA/ZCL)"],
-//   "profile":"Home Automation (HA)",
-//   "name": "Iris Plug One",
-//   "device": "Iris Plug",
-//   "type": "binaryswitches"
-// }, {
-//   "network-id":"093B",
-//   "endpoint":"01",
-//   "euid64":"000D6F000B2E5846",
-//   "in-clusters":["Basic","Identify","Groups","Scenes","On/Off","Electricity Measurement","Diagnostics","FC03[Unknown/Non-Standard]"],
-//   "out-clusters":["OTA Update (Non HA/ZCL)"],
-//   "profile":"Home Automation (HA)",
-//   "name": "Leviton Plug One",
-//   "device": "Leviton Plug",
-//   "type": "binaryswitches"
-// }, {
-//   "network-id":"093B",
-//   "endpoint":"01",
-//   "euid64":"000D6F000B2E5846",
-//   "in-clusters":["Basic","Identify","Groups","Scenes","On/Off","Electricity Measurement","Diagnostics","FC03[Unknown/Non-Standard]"],
-//   "out-clusters":["OTA Update (Non HA/ZCL)"],
-//   "profile":"Home Automation (HA)",
-//   "name": "Yale Lock One",
-//   "device": "Yale Lock",
-//   "type": "locks"
-// }, {
-//   "network-id":"093B",
-//   "endpoint":"01",
-//   "euid64":"000D6F000B2E5846",
-//   "in-clusters":["Basic","Identify","Groups","Scenes","On/Off","Electricity Measurement","Diagnostics","FC03[Unknown/Non-Standard]"],
-//   "out-clusters":["OTA Update (Non HA/ZCL)"],
-//   "profile":"Home Automation (HA)",
-//   "name": "Iris Plug Two",
-//   "device": "Iris Plug",
-//   "type": "binaryswitches"
-// }, {
-//   "network-id":"093B",
-//   "endpoint":"01",
-//   "euid64":"000D6F000B2E5846",
-//   "in-clusters":["Basic","Identify","Groups","Scenes","On/Off","Electricity Measurement","Diagnostics","FC03[Unknown/Non-Standard]"],
-//   "out-clusters":["OTA Update (Non HA/ZCL)"],
-//   "profile":"Home Automation (HA)",
-//   "name": "Iris Plug Three",
-//   "device": "Iris Plug",
-//   "type": "binaryswitches"
-// }];
-//
-// function incrementDevices(obj) {
-//   if(obj.device === "Iris Plug") {
-//     irisCount ++;
-//   }
-//   if(obj.device === "Leviton Plug") {
-//     levitonCount ++;
-//   }
-//   if(obj.device === "Yale Lock") {
-//     yaleCount ++;
-//   }
-// }
-//
-// function resetDeviceCount() {
-//   irisCount = 0;
-//   levitonCount = 0;
-//   yaleCount = 0;
-// }
-//
-// function makeFirstDiv(divId, cssClass) {
-//   return '<div id="' + divId + '" class="dap-control-element dap-toggle-' + cssClass + '"> </div>';
-// }
-//
-// function makeSecondDiv(id) {
-//   return '<div class="dap-box ' + id + '-dap-box"> </div>';
-// }
-//
-// function makeFirstP(id, text) {
-//   return '<p class="dap-title ' + id + '-dap-title">' + text + '</p>';
-// }
-//
-// function makeThirdDiv(id) {
-//   return '<div class="div-three ' + id + '-div-three"> </div>';
-// }
-//
-// function makeSecondP(id, text) {
-//   return '<p class="dap-box-text ' + id + '-dap-box-text">' + text + '</p>';
-// }
-//
-// function makeFirstI(text) {
-//   return '<i class="fa fa-' + text + ' fa-3x" aria-hidden="true"></i>';
-// }
-//
-// function elementFromObj(obj, number) {
-//   if(obj.device === "Iris Plug") {
-//     var divId = "lightBtn-" + number;
-//     var cssClass = "iris-plug";
-//     var fontAwesomeClass = "lightbulb-o";
-//     var secondPText = "Off";
-//   }
-//   if(obj.device === "Leviton Plug") {
-//     var divId = "lampBtn-" + number;
-//     var cssClass = "leviton-plug";
-//     var fontAwesomeClass = "lightbulb-o";
-//     var secondPText = "Off";
-//   }
-//   if(obj.device === "Yale Lock") {
-//     var divId = "doorBtn-" + number;
-//     var cssClass = "yale-lock";
-//     var fontAwesomeClass = "unlock";
-//     var secondPText = "Unlocked";
-//   }
-//   console.log("makeFirstDiv: ", makeFirstDiv(divId, cssClass));
-//
-//   var firstDiv = document.createElement("div");
-//   document.querySelector('.obj-element-container').appendChild(firstDiv);
-//   firstDiv.outerHTML = makeFirstDiv(divId, cssClass);
-//
-//   var secondDiv = document.createElement("div");
-//   document.getElementById(divId).appendChild(secondDiv);
-//   secondDiv.outerHTML = makeSecondDiv(divId);
-//
-//   var firstP = document.createElement("p");
-//   document.getElementById(divId).appendChild(firstP);
-//   firstP.outerHTML = makeFirstP(divId, obj.name);
-//
-//   var thirdDiv = document.createElement("div");
-//   document.querySelector('.' + divId + '-dap-box').appendChild(thirdDiv);
-//   thirdDiv.outerHTML = makeThirdDiv(divId);
-//
-//   var secondP = document.createElement("p");
-//   document.querySelector('.' + divId + '-dap-box').appendChild(secondP);
-//   secondP.outerHTML = makeSecondP(divId, secondPText);
-//
-//   var firstI = document.createElement("i");
-//   document.querySelector('.' + divId + '-div-three').appendChild(firstI);
-//   firstI.outerHTML = makeFirstI(fontAwesomeClass);
-//
-// }
-//
-// // elementFromObj(exampleObj, 1);
-//
-// exampleObjArray.forEach(function (c,i,a) {
-//   incrementDevices(c);
-//   if (c.device === "Iris Plug") {
-//     elementFromObj(c, irisCount);
-//   }
-//   if (c.device === "Leviton Plug") {
-//     elementFromObj(c, levitonCount);
-//   }
-//   if (c.device === "Yale Lock") {
-//     elementFromObj(c, yaleCount);
-//   }
-// });
-//
-// resetDeviceCount();
+
+    console.log('makeFirstDiv: ', makeFirstDiv(c.gender));
+    console.log('i: ', i);
+
+
+  });
+}
